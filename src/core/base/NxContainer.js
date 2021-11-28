d2693f1579ad494a4a50d3438f5dcc1e02abded1
I-is-as-I-does <vir.heroicus@gmail.com>
2021-11-28 23:23:50 +0100

    debug/history/edit opts in url | 1st starter kit

--- src/core/base/NxContainer.js
+++ src/core/base/NxContainer.js
@@ -5,6 +5,16 @@ import { setOptions } from "./NxOptions.js";
 var container;
 var host;
 
+function dynmOpts(containerOpts){
+var params = new URLSearchParams(window.location.search.slice(1));
+["edit","debug", "history"].forEach(p => {
+if(params.has(p)){
+  containerOpts[p] = true;
+}
+});
+        setOptions(containerOpts);
+}
+
 export function setContainer(selector = null) {
 
     if(!container){
@@ -13,13 +23,14 @@ export function setContainer(selector = null) {
     }
     container = document.querySelector(selector);
    
+    var containerOpts = {};
     if (!container) {
       container = document.createElement("DIV");
       document.body.append(container);
-    } else if (container.dataset) {
-     
-            setOptions(Object.assign({},container.dataset));
+    } else if (container.dataset) {    
+      containerOpts = Object.assign(containerOpts,container.dataset);
     }
+    dynmOpts(containerOpts);
     host = document.createElement('DIV');
     host.className = "nx";
     container.append(host);
