736669f4078089fb5750eac595e6f820e23467f9
I-is-as-I-does <vir.heroicus@gmail.com>
2021-12-12 19:10:12 +0100

    wip repo reorg

--- .fridge/_bits/NxEditorIO.js
+++
@@ -1,8 +0,0 @@
-(function () {
-      import("../editor/NxEditorInstance.js").then((NxEditor)=> { 
-      import("../page/NxInit.js").then((NxInit)=> { 
-        NxInit.initPage(NxEditor.editorElms);
-        });
-      });
-    })();
-    
--- .fridge/_bits/NxViewerIO.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  import("../viewer/NxViewerInstance.js").then((NxViewer)=> { 
-  import("../core/load/NxInit.js").then((NxInit)=> { 
-    NxInit.initPage(NxViewer.viewerElms);
-    });
-  });
-})();
